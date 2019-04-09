b8e0e7d528510f8723706d759195c3e9a9905277
donw385 <donw385@users.noreply.github.com>
2019-04-09 18:44:57 -0400

    done day 2

--- twitoff/models.py
+++ twitoff/models.py
@@ -7,6 +7,7 @@ class User(DB.Model):
     """Twitter users that we pull and analyze Tweets for."""
     id = DB.Column(DB.BigInteger,primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
@@ -14,7 +15,8 @@ class User(DB.Model):
 class Tweet(DB.Model):
     """Tweets."""
     id = DB.Column(DB.BigInteger,primary_key=True)
-    text = DB.Column(DB.Unicode(280))
+    text = DB.Column(DB.Unicode(500))
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
